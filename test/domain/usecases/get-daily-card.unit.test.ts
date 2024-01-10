a4b6273e8e58da4a75309bbffcf7a364722dfe6e
NGustavo011 <gsn181200@gmail.com>
2024-01-10 14:38:46 -0300

    refactor: implemented mockSavedCard from another file

--- test/domain/usecases/get-daily-card.unit.test.ts
+++ test/domain/usecases/get-daily-card.unit.test.ts
@@ -1,14 +1,6 @@
-import {
-	describe,
-	test,
-	jest,
-	beforeAll,
-	expect,
-	spyOn,
-	beforeEach,
-} from "bun:test";
-import { SavedCard } from "../../../src/domain/entities/card";
+import { describe, test, jest, beforeAll, expect, beforeEach } from "bun:test";
 import { GetDailyCard } from "../../../src/domain/usecases/get-daily-card";
+import { mockSavedCard } from "../../mocks/mock-card";
 
 const throwError = (): never => {
 	throw new Error();
@@ -19,25 +11,6 @@ describe("GetDailyCard usecase", () => {
 		getDailyCard: jest.Mock;
 	};
 	let sut: GetDailyCard;
-
-	const mockSavedCard = (): SavedCard => {
-		return new SavedCard({
-			id: "any_id",
-			name: "any_name",
-			race: "any_race",
-			type: "any_type",
-			archetype: "any_archetype",
-			attribute: "any_attribute",
-			description: "any_description",
-			frameType: "any_frame_type",
-			imageUrl: "any_image_url",
-			imageUrlSmall: "any_image_url_small",
-			imageUrlCropped: "any_image_url_cropped",
-			atk: 0,
-			def: 0,
-			level: 0,
-		});
-	};
 
 	beforeAll(() => {
 		getDailyCardRepository = {
