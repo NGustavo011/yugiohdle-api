2e524e0f587d9ee33b2a551ce8bf4d828990d4bd
NGustavo011 <gsn181200@gmail.com>
2024-01-31 14:05:27 -0300

    feat: add reset cache cron in .env

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -5,5 +5,6 @@ export default {
 	cacheUrl: process.env.CACHE_URL ?? "redis://localhost:6379",
 	cacheDailyCardKey: process.env.CACHE_DAILY_CARD_KEY ?? "DailyCard",
 	cacheCardsKey: process.env.CACHE_CARDS_KEY ?? "Cards",
+	cacheResetCron: process.env.CACHE_RESET_CRON ?? "0 0 * * *",
 	port: process.env.PORT ?? 3333,
 };
--- src/main/cron-jobs/change-daily-card-cron.ts
+++ src/main/cron-jobs/change-daily-card-cron.ts
@@ -2,11 +2,12 @@ import cron from "@elysiajs/cron";
 import Elysia from "elysia";
 import { adaptElysiaCron } from "../adapters/elysia-cron-adapter";
 import { makeChangeDailyCardController } from "../factories/controllers/change-daily-card-controller-factory";
+import env from "../config/env";
 
 export const changeDailyCardCron = new Elysia().use(
 	cron({
 		name: "change daily card",
-		pattern: "0 0 * * *",
+		pattern: env.cacheResetCron,
 		run() {
 			adaptElysiaCron(makeChangeDailyCardController());
 		},
